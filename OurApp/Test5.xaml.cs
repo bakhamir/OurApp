b24c94c41163e3100fcbb5513bcf405fb3e6b68d
БахытжановА <@207-9>
2023-12-08 21:42:57 +0600

    допилил последние окна осталось разобраться с результатом

--- OurApp/Test5.xaml.cs
+++ OurApp/Test5.xaml.cs
@@ -23,5 +23,35 @@ namespace OurApp
         {
             InitializeComponent();
         }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton RadButt = (RadioButton)sender;
+            if (RadButt.IsChecked == true)
+            {
+                MainWindow.Points++;
+                MessageBox.Show("Верно");
+            }
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            RadioButton RadButt = (RadioButton)sender;
+            if (RadButt.IsChecked == true)
+            {
+                MainWindow.Points++;
+                MessageBox.Show("Верно");
+            }
+        }
+
+        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
+        {
+            RadioButton RadButt = (RadioButton)sender;
+            if (RadButt.IsChecked == true)
+            {
+                MainWindow.Points++;
+                MessageBox.Show("Верно");
+            }
+        }
     }
 }
